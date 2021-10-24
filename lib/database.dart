51f295233baa37f34aa4b2b64f402ce38a2cbbe1
dudinha21 <65730044+dudinha21@users.noreply.github.com>
2021-10-24 11:22:06 -0300

    Update database.dart

--- lib/database.dart
+++ lib/database.dart
@@ -2,12 +2,13 @@ class DatabaseHelper{
 
       initDB() async{
          String databasePath = await getDatabasesPath();
-         String path = join(databasePath, "usuarios".db)
+         String path = join(databasePath, "usuarios.db")
 
          var db = await openDatabase(
          path,
          version: 1,
          onCreate: _onCreate,
+               
          );
 
          return db;
@@ -15,30 +16,11 @@ class DatabaseHelper{
 
       FutureOr<void>_onCreate( database db, int version){
          
-          String sql = 'CREATE TABLE usuarios (
-          id INT PRIMARY KEY AUTO_INCREMENT, 
-          nome VARCHAR(200), 
-          cpf VARCHAR(14), 
-          email VARCHAR(100), 
-          cidade VARCHAR(100),
-          uf VARCHAR(2),
-          data_nascimento VARCHAR(10),
-          sexo VARCHAR(20),
-          senha VARCHAR(50),
-          numero_cartao_sus INT
-          );'
-          ;
+          String sql = 'CREATE TABLE usuarios (id INT PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(200), cpf VARCHAR(14), email VARCHAR(100), cidade VARCHAR(100), uf VARCHAR(2), data_nascimento VARCHAR(10), sexo VARCHAR(20), senha VARCHAR(50), numero_cartao_sus INT);';
           await db.execute(sql);
 
 
-          sql = 'CREATE TABLE agendamento(
-          id_agendamento INT PRIMARY KEY AUTO_INCREMENT, 
-          local VARCHAR(100),
-          dia VARCHAR(20),
-          horario VARCHAR(20)
-          id_usuario INT,
-          CONSTRAINT fk_usuario_agenda FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
-          );'
+          sql = 'CREATE TABLE agendamento(id_agendamento INT PRIMARY KEY AUTO_INCREMENT, local VARCHAR(100), dia VARCHAR(20), horario VARCHAR(20), id_usuario INT, CONSTRAINT fk_usuario_agenda FOREIGN KEY(id_usuario) REFERENCES usuarios(id));';
           await db.execute(sql);
 
       }
